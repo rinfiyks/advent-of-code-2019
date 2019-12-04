d68c21e869acab54ec72123f678415fc5cbf360b
rinfiyks <tom.gould.92@gmail.com>
2019-12-04 22:16:38 +0000

    Test names

--- src/test/kotlin/puzzles/Day4Test.kt
+++ src/test/kotlin/puzzles/Day4Test.kt
@@ -6,14 +6,14 @@ import org.junit.jupiter.api.Test
 internal class Day4Test {
 
     @Test
-    fun part1Criteria() {
+    fun testPart1Criteria() {
         assertThat(Day4.part1Criteria(111111)).isTrue()
         assertThat(Day4.part1Criteria(223450)).isFalse()
         assertThat(Day4.part1Criteria(123789)).isFalse()
     }
 
     @Test
-    fun part2Criteria() {
+    fun testPart2Criteria() {
         assertThat(Day4.part2Criteria(112233)).isTrue()
         assertThat(Day4.part2Criteria(123444)).isFalse()
         assertThat(Day4.part2Criteria(111122)).isTrue()
