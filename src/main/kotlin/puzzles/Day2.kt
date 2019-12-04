d4851674e387ff5677ebe551bc8484100479d79f
rinfiyks <tom.gould.92@gmail.com>
2019-12-04 13:12:42 +0000

    Companion object

--- src/main/kotlin/puzzles/Day2.kt
+++ src/main/kotlin/puzzles/Day2.kt
@@ -19,30 +19,32 @@ class Day2(rawInput: List<String>) {
         return 0
     }
 
-    tailrec fun runProgram(program: List<Int>, pointer: Int = 0): List<Int> {
-        return when (program[pointer]) {
-            1 -> {
-                val updatedProgram = applyOpcode(program, pointer, Int::plus)
-                runProgram(updatedProgram, pointer + 4)
+    companion object {
+        tailrec fun runProgram(program: List<Int>, pointer: Int = 0): List<Int> {
+            return when (program[pointer]) {
+                1 -> {
+                    val updatedProgram = applyOpcode(program, pointer, Int::plus)
+                    runProgram(updatedProgram, pointer + 4)
+                }
+                2 -> {
+                    val updatedProgram = applyOpcode(program, pointer, Int::times)
+                    runProgram(updatedProgram, pointer + 4)
+                }
+                else -> program
             }
-            2 -> {
-                val updatedProgram = applyOpcode(program, pointer, Int::times)
-                runProgram(updatedProgram, pointer + 4)
-            }
-            else -> program
         }
-    }
 
-    private fun applyOpcode(program: List<Int>, index: Int, op: (Int, Int) -> Int): List<Int> {
-        val x = program[program[index + 1]]
-        val y = program[program[index + 2]]
-        return program.updated(program[index + 3], op(x, y))
-    }
+        private fun applyOpcode(program: List<Int>, index: Int, op: (Int, Int) -> Int): List<Int> {
+            val x = program[program[index + 1]]
+            val y = program[program[index + 2]]
+            return program.updated(program[index + 3], op(x, y))
+        }
 
-    private fun <T> List<T>.updated(index: Int, value: T): List<T> {
-        val m = this.toMutableList()
-        m[index] = value
-        return m.toList()
+        private fun <T> List<T>.updated(index: Int, value: T): List<T> {
+            val m = this.toMutableList()
+            m[index] = value
+            return m.toList()
+        }
     }
 
 }
--- src/test/kotlin/puzzles/Day2Test.kt
+++ src/test/kotlin/puzzles/Day2Test.kt
@@ -7,21 +7,19 @@ internal class Day2Test {
 
     @Test
     fun testRunProgram() {
-        val day2 = Day2(listOf("0"))
-
-        assertThat(day2.runProgram(listOf(1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50)))
+        assertThat(Day2.runProgram(listOf(1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50)))
             .isEqualTo(listOf(3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50))
 
-        assertThat(day2.runProgram(listOf(1, 0, 0, 0, 99)))
+        assertThat(Day2.runProgram(listOf(1, 0, 0, 0, 99)))
             .isEqualTo(listOf(2, 0, 0, 0, 99))
 
-        assertThat(day2.runProgram(listOf(2, 3, 0, 3, 99)))
+        assertThat(Day2.runProgram(listOf(2, 3, 0, 3, 99)))
             .isEqualTo(listOf(2, 3, 0, 6, 99))
 
-        assertThat(day2.runProgram(listOf(2, 4, 4, 5, 99, 0)))
+        assertThat(Day2.runProgram(listOf(2, 4, 4, 5, 99, 0)))
             .isEqualTo(listOf(2, 4, 4, 5, 99, 9801))
 
-        assertThat(day2.runProgram(listOf(1, 1, 1, 4, 99, 5, 6, 0, 99)))
+        assertThat(Day2.runProgram(listOf(1, 1, 1, 4, 99, 5, 6, 0, 99)))
             .isEqualTo(listOf(30, 1, 1, 4, 2, 5, 6, 0, 99))
     }
 
